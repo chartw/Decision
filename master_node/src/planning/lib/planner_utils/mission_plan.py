0fe7dc12922c5fc9dcfe050c274f8637971356fb
sh-song <shs@inha.edu>
2021-07-03 18:45:04 +0900

    mission plan modify

--- master_node/src/planning/lib/planner_utils/mission_plan.py
+++ master_node/src/planning/lib/planner_utils/mission_plan.py
@@ -2,29 +2,38 @@ class MissonPlan:
     def __init__(self,planner):
         self.obstacles = planner.obstacles
         self.objects=planner.objects
-        self.position=planner.position
+        self.local=planner.local
         self.surface_msg = planner.surface_msg
         self.serial_msg = planner.serial_msg
         self.mode=""
+        self.mission_ing = planner.mission_ing # True / False
 
-    def decision(self):
+
+
+    def decision(self):  
         
         
-        if 1 is 2:
-            pass
-        
+        if 1 is 2: # Parking
+            self.mode = 'parking'
+            self.mission_ing = True
+            
         elif planner.surface_msg is "stopline" and self.serial_msg.speed > 10 and abs(self.srial_msg.steer) < 5:
             self.mode = 'normal_stop'
+            self.mission_ing = True
             
-        elif 2 is 2:
-            pass
+        elif self.local.x is coordinate: # Dyanamic -- person
+            self.mode = 'emergency_stop'
+            self.mission_ing = True
         
-        elif 3 is 3:
-            pass
+        elif self.local.x is 3: # Static -- cone
+            self.mode = 'avoidance'
+            self.mission_ing = True
+
+        elif 4 is 4: 
+            self.mission_ing = True
         
         else:
             self.mode = 'general'
-       
 
 
         return self.mode
