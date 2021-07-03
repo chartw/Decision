b665daea21e37c17117f0c6164fed871d682ee81
sh-song <shs@inha.edu>
2021-07-03 19:32:55 +0900

    add mission plan for static and dynamic obstacle avoidance

--- master_node/src/planning/lib/planner_utils/mission_plan.py
+++ master_node/src/planning/lib/planner_utils/mission_plan.py
@@ -1,3 +1,5 @@
+from math import hypot
+
 class MissonPlan:
     def __init__(self,planner):
         self.obstacles = planner.obstacles
@@ -11,21 +13,22 @@ class MissonPlan:
 
 
     def decision(self):  
-        
-        
+
         if 1 is 2: # Parking
             self.mode = 'parking'
             self.mission_ing = True
             
-        elif planner.surface_msg is "stopline" and self.serial_msg.speed > 10 and abs(self.srial_msg.steer) < 5:
+        elif self.surface_msg is "stopline" and self.serial_msg.speed > 10 and abs(self.srial_msg.steer) < 5:
             self.mode = 'normal_stop'
             self.mission_ing = True
-            
-        elif self.local.x is coordinate: # Dyanamic -- person
+
+        # Dyanamic -- person stop at node 24    
+        elif hypot(self.local.x - 2.125, self.local.y - 43.617) < 1:
             self.mode = 'emergency_stop'
             self.mission_ing = True
-        
-        elif self.local.x is 3: # Static -- cone
+            
+        # Static -- cone avoidance at node 16
+        elif hypot(self.local.x - 29.757, self.local.y - 35.737) < 1: 
             self.mode = 'avoidance'
             self.mission_ing = True
 
