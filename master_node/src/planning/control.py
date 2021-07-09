0f31c921309dd1b214d61be7878471063549ac66
NUVO <12161800@inha.edu>
2021-07-09 21:13:01 +0900

    Uturn 추가

--- master_node/src/planning/control.py
+++ master_node/src/planning/control.py
@@ -1,10 +1,13 @@
 import rospy
+import time
 
 from master_node.msg import Path, Serial_Info, Planning_Info, Local  # 개발할 메세지 타입
 from lib.control_utils.general import General
 from lib.control_utils.avoidance import Avoidance
 # from lib.control_utils.emergency_stop import EmergencyStop
 from lib.control_utils.normal_stop import NormalStop
+
+from lib.control_utils.uturn import Uturn
 
 """
 Serial_Info
@@ -25,7 +28,8 @@ Planning_Info
     Point32 point
 }
 """
-
+LEFT_MAX_STEER = 1999
+RIGHT_MAX_STEER = -1999
 
 class Control:
     def __init__(self):
@@ -47,6 +51,7 @@ class Control:
         avoidance = Avoidance(self)
         # emergency_stop = EmergencyStop(self)
         self.normal_stop = NormalStop(self)
+        self.uturn = Uturn(self)
         self.is_planning = False
 
         rate = rospy.Rate(50)  # 100hz
@@ -58,6 +63,10 @@ class Control:
             print("#####", self.serial_info)
             # rospy.Subscriber("/serial", Serial_Info, self.serialCallback) # 여기서 지금 받은거._ 현재  SERIAL 상태.
             # rospy.Subscriber("/planner", Planning_Info, self.planningCallback)
+
+
+
+
 
             if self.is_planning:
                 if self.planning_info.mode == "general":
@@ -72,6 +81,27 @@ class Control:
                         # print(1)
                         self.pub_msg = self.general.driving()
                         print("### pub", self.pub_msg)
+
+
+            timetime=time.time()+3
+            if self.planning_info.mode == "uturn":
+
+
+                # steering value 를 serial로 넣어주기
+                self.pub_msg.steer = LEFT_MAX_STEER
+
+                
+                
+                # 3초간 유지
+                curtime=time.time()
+                if(curtime<time)
+                # 새로운 점 잡기
+                new_idx = self.uturn.select_new_target(self.local, self.global_path)
+
+                # GPP 경로 재 생성 (new idx 경유)
+
+
+
 
 
                 # elif self.planning_info.mode == "avoidance":
@@ -122,7 +152,8 @@ class Control:
         self.serial_info.brake = msg.brake
 
 
-        # print(self.serial_info) # 얜 잘 받음 / 근데  general 에서 못받아.ㅇㄹ이러이라ㅓㅁ댜ㅐ렁마러ㅑㅐㄷ머랑ㅁ르
+    # def missionCallback(self, msg):
+    #     self.
 
 
 control = Control()
---
+++ master_node/src/planning/lib/control_utils/uturn.py
@@ -0,0 +1,35 @@
+from os import O_NOATIME
+
+
+MAX_VALUE = 10000
+
+class Uturn:
+    def __init__(self):
+        self.none = "NONE"
+
+    def calc_dis(self, a_x, a_y, b_x, b_y):
+        distance = ((a_x - b_x)**2 +  (a_y - b_y)**2)**0.5
+        return distance
+
+    def sameDirection(self, local, global_path):
+        if local.yaw - global_path.yaw < 5:
+            return True
+        else:
+            return False
+
+    def select_new_target(self, local, global_path):
+        
+        distance_min = MAX_VALUE
+        min_idx = MAX_VALUE
+
+        for i in len(global_path):
+            temp_dis = self.calc_dis(global_path[i].x, global_path[i].y, local.x, local.y)
+            if temp_dis < distance_min and self.sameDirection(local, global_path[i]):
+                distance_min = temp_dis
+                min_idx = i
+
+        return min_idx
+
+
+    
+        
