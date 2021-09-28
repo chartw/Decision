e69acbd615833e363303a4070cf341840a1a6ae1
Taewoong Cha <12161800@inha.edu>
2021-09-29 02:18:18 +0900

    calibration.py publish 완료

--- master_node/src/lidar/calibration.py
+++ master_node/src/lidar/calibration.py
@@ -1,10 +1,9 @@
 import rospy
 from sensor_msgs.msg import PointCloud
-from master_node.msg import Obstacles
-from darknet_ros_msgs.msg import BoundingBoxes
+from geometry_msgs.msg import Point32
+from master_node.msg import Obstacles, CalibObjects
 from yolov4_trt_ros.msg import Detector2DArray
 from yolov4_trt_ros.msg import Detector2D
-from darknet_ros_msgs.msg import BoundingBox
 from shapely.geometry import Point, Polygon
 from math import sin, cos, radians
 import numpy as np
@@ -12,7 +11,8 @@ import numpy as np
 class calibration:
     def __init__(self):
         rospy.init_node("calibration", anonymous=False)
-        delivery_publish = rospy.Publisher("delivery_cali", Obstacles, queue_size=1)
+        self.calib_pub = rospy.Publisher("/calib_object", CalibObjects, queue_size=1)
+        self.pub_msg=CalibObjects
         # outlier_publish = rospy.Publisher("outlier_obs", Obstacles(), queue_size=1)
         rospy.Subscriber("lidar_pub", PointCloud, self.get_lidar)
         # rospy.Subscriber("obstacles", Obstacles, self.get_obstacles)
@@ -46,6 +46,7 @@ class calibration:
     def point_process(self):
         bbox_space_poly = []
         bbox_temp_xyzcnt = []
+        self.pub_msg=CalibObjects()
         
         if self.lidar_flag and self.bbox_flag:
             for i in range(len(self.bbox_msg)):
@@ -79,83 +80,17 @@ class calibration:
                     bbox_temp_xyzcnt[i][0] = bbox_temp_xyzcnt[i][0]/bbox_temp_xyzcnt[i][3]
                     bbox_temp_xyzcnt[i][1] = bbox_temp_xyzcnt[i][1]/bbox_temp_xyzcnt[i][3]
                     bbox_temp_xyzcnt[i][2] = bbox_temp_xyzcnt[i][2]/bbox_temp_xyzcnt[i][3]
-                    print(self.bbox_msg[i].results.id, bbox_temp_xyzcnt[i][0], bbox_temp_xyzcnt[i][1], bbox_temp_xyzcnt[i][2], bbox_temp_xyzcnt[i][3])
-            
+                    point=Point32(bbox_temp_xyzcnt[i][0], bbox_temp_xyzcnt[i][1], bbox_temp_xyzcnt[i][2])
+                    self.pub_msg.points.append(point)
+                    self.pub_msg.Class.append(self.bbox_msg[i].results.id)
+        
+        self.pub_msg.header.stamp=rospy.Time.now()
+        self.pub_msg.header.frame_id = "world"
+
+        self.calib_pub.publish(self.pub_msg)
+
         self.lidar_flag = False
         self.bbox_flag = False
-
-        
-    def circle_z(self):
-        obstacle_space_poly = []
-        obs_z = []
-
-        for i in range(len(self.obstacle_msg.circles)):
-            x1, x2 = self.obstacle_msg.circles[i].center.x - self.obstacle_msg.circles[i].radius, self.obstacle_msg.circles[i].center.x + self.obstacle_msg.circles[i].radius
-            y1, y2 = self.obstacle_msg.circles[i].center.y - self.obstacle_msg.circles[i].radius, self.obstacle_msg.circles[i].center.y + self.obstacle_msg.circles[i].radius
-
-            obstacle_space = [[x1, y1], [x1, y2], [x2, y2], [x2, y1]]
-            obstalce_temp = Polygon(obstacle_space)
-            obstacle_space_poly.append(obstalce_temp)
-
-        for i in range(len(self.lidar_msg.points)):
-            obs_z.append([0,0])
-
-        for i in range(len(self.lidar_msg.points)):
-            test_point = Point(self.lidar_msg.points[i].x, self.lidar_msg.points[i].y)
-            for p in range(len(obstacle_space_poly)):
-                if test_point.within(obstacle_space_poly[p]):
-                    obs_z[p][0] += self.lidar_msg.points[i].z
-                    obs_z[p][1] += 1
-
-        for i in range(len(obs_z)):
-            if obs_z[i][1] == 0:
-                obs_z[i][1] = 1
-
-        for i in range(len(self.obstacle_msg.circles)):
-            self.obstacle_msg.circles[i].center.z = obs_z[i][0] / obs_z[i][1]
-        
-        self.calibration()
-    
-    def calibration(self):
-        atf_cali = []
-        bbox_space_poly = []
-        
-        # print(self.bbox_msg[1])
-        
-        if self.flag:
-            self.inner_temp = Obstacles()
-            self.outlier_temp = Obstacles()
-
-            # print(self.bbox_msg)
-            # print(self.bbox_msg[0].xmin)
-            for i in range(len(self.bbox_msg)):
-                bbox_space = [[self.bbox_msg[i].xmin, self.bbox_msg[i].ymin], [self.bbox_msg[i].xmin, self.bbox_msg[i].ymax], [self.bbox_msg[i].xmax, self.bbox_msg[i].ymax], [self.bbox_msg[i].xmax, self.bbox_msg[i].ymin]]
-                bbox_temp = Polygon(bbox_space)
-                bbox_space_poly.append(bbox_temp)
-
-            for i in range(len(self.obstacle_msg.circles)):
-                c_x = -self.obstacle_msg.circles[i].center.y
-                c_y = 1.13 - self.obstacle_msg.circles[i].center.z
-                c_z = 0.6 + self.obstacle_msg.circles[i].center.x
-                obs_matrix = np.array([[c_x], [c_y], [c_z]])
-
-                pixel_matrix = np.dot(self.camera_matrix, obs_matrix)
-
-                # print(pixel_matrix.shape)
-                print("obs :", i)
-                print(pixel_matrix[0]/pixel_matrix[2], pixel_matrix[1]/pixel_matrix[2], pixel_matrix[2]/pixel_matrix[2])
-                cali_point = Point(pixel_matrix[0]/pixel_matrix[2], pixel_matrix[1]/pixel_matrix[2])
-
-                for p in range(len(bbox_space_poly)):
-                    if cali_point.within(bbox_space_poly[p]):
-                        if self.bbox_msg[p].id == 'corn':
-                            self.inner_temp.circles.append(self.obstacle_msg.circles[i])
-                        else:
-                            self.outlier_temp.circles.append(self.obstacle_msg.circles[i])            
-                        # print(self.bbox_msg[p].id)
-                        # print(self.bbox_msg[p].Class)
-                self.inner_publish.publish(self.inner_temp)
-                self.outlier_publish.publish(self.outlier_temp)
                 
 print("ON") 
 Calibration = calibration()       
