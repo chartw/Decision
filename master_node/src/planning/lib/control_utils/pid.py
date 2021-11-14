c4fe3b51a8484bb789be17edf653fb92910060e9
TaeWoong Cha <12161800@inha.edu>
2021-11-14 20:51:02 +0900

    pid.py 수정

--- master_node/src/planning/lib/control_utils/pid.py
+++ master_node/src/planning/lib/control_utils/pid.py
@@ -1,24 +1,21 @@
-
-
 class PID:
     def __init__(self):
-        self.P = 0.4
-        self.I = 0.0002
-        self.D = 0.002
+        self.P = 2.0
+        self.I = 0.04
+        self.D = 2.0
         self.pre_error = 0.0
         self.error_sum = 0.0
         self.dt = 1.0 / 10.0
 
-    def run(self, target, current):
-        if target == 0.0 and current < 1.0 / 3.6:
+    def run(self, target, current, Vcurrent):
+        if Vcurrent < 1:
             self.pre_error = 0.0
             self.error_sum = 0.0
-            return -0.7
+            return -0.7  # 어떤값으로 주는게 좋을지 찾아야함
         else:
-            error = target - current
+            error = current - target
 
-            
             diff_error = error - self.pre_error
             self.pre_error = error
             self.error_sum += error
-            return self.P*error + self.D*diff_error/self.dt + self.I*self.error_sum*self.dt
+            return self.P * error + self.D * diff_error / self.dt + self.I * self.error_sum * self.dt
